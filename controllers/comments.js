84d7ee4b38332b286401317ccc567a3ade76766d
AFresnedo <AFresnedo@users.noreply.github.com>
2018-08-21 13:13:37 -0700

    activate comments post route

--- controllers/comments.js
+++ controllers/comments.js
@@ -3,10 +3,11 @@ var router = express.Router();
 var db = require('../models');
 
 router.post('/', function(req, res) {
+  console.log('create comment request recieved');
   db.comment.create(req.body).then(function(createdComment) {
     res.redirect('/articles/' + req.body.articleId);
   }).catch(function(err) {
-    res.render('error');
+    res.send('error creating comment');
   });
 });
 
