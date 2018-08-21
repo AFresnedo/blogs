f6d038ad428a45fd5b5ce9051e47a65312de1aa8
AFresnedo <AFresnedo@users.noreply.github.com>
2018-08-21 15:16:30 -0700

    race condition discovered in article post, related to tags

--- controllers/articles.js
+++ controllers/articles.js
@@ -47,7 +47,31 @@ router.post('/', function(req, res) {
   if (req.body.authorId !== 0) {
     console.log(req.body);
     db.article.create(req.body).then(function(createdArticle){
-      res.redirect('/articles/' + createdArticle.id);
+      // do tag stuff
+      var tags = [];
+      if (req.body.tags) {
+        // in our current impl, tags are seperated by comma
+        tags = req.body.tags.split(',');
+      }
+      if (tags.length > 0) {
+        // add the tags
+        tags.forEach(function(t) {
+          db.tag.findOrCreate({
+            where: { name: t.trim() }
+          }).spread(function(newTag, wasCreated) {
+            // ^ newTag was either found or just created
+            // sequelize helper method for attaching a tag to an article
+            // where tag belogns to article
+            // below is the entry into the join table
+            createdArticle.addTag(newTag);
+          });
+        });
+        // TODO OH NO A RACE DONDITION i didn't see it wahhhhhh
+        res.redirect('/articles/' + createdArticle.id);
+      }
+      else {
+        res.redirect('/articles/' + createdArticle.id);
+      }
     }).catch(function(err) {
       console.log(err);
       res.send('article create failed');
